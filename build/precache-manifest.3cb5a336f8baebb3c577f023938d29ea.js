310d945623f67f6bc0a2db1790524ee1faf5c3ef
jerrol <jerrol.brooks@nagrastar.com>
2019-08-13 08:30:10 -0600

    chore: update .gitignore

---
+++ build/precache-manifest.3cb5a336f8baebb3c577f023938d29ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f41dfe6229555e4345243857239f8a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a6d30618ec30e0b4771",
+    "url": "/static/css/main.6dc7eb0c.chunk.css"
+  },
+  {
+    "revision": "a0ef04eb616c3eba8a13",
+    "url": "/static/js/2.124a0595.chunk.js"
+  },
+  {
+    "revision": "8a6d30618ec30e0b4771",
+    "url": "/static/js/main.d4b5d756.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
