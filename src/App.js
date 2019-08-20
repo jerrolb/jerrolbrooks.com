d07fb9b66f16f6c75230a15793fa45e27d9ab209
jerrol <jerrol.brooks@nagrastar.com>
2019-08-20 14:46:51 -0600

    feat: cleanup, add layout and header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ROUTES } from './config/constants';
+import { Header } from './components/Header.js';
+import { Layout } from 'antd';
+import { Game } from './games/tictactoe/tictactoe.js';
+// import './App.css';
+const { Sider, Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>NOT Loading!!!</p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    ticTacToe = () => {
+        return <Game />;
+    }
+
+    diceRoller = () => {
+        // return <DiceRoller />;
+        return <p>Dice Roller Coming Soon</p>;
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <Router>
+                    <Layout>
+                        <header className="App-header">
+                            <Header />
+                        </header>
+                        <Layout>
+                            <Sider></Sider>
+                            <Content>
+                                {/* <Route path={ ROUTES.HOME } component={ this.home } /> */}
+                                <Route path={ ROUTES.TICTACTOE } component={ this.ticTacToe } />
+                                <Route path={ ROUTES.DICEROLLER } component={ this.diceRoller } />
+                            </Content>
+                        </Layout>
+                        <footer>
+                            <Footer></Footer>
+                        </footer>
+                    </Layout>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
