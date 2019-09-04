5efcf338052214be4486638f85ab606d6e4e2b32
jerrol <jerrol.brooks@nagrastar.com>
2019-09-04 11:57:14 -0600

    refactor: cleanup, use string interpolation, use react refs

--- src/games/diceroller/dice.js
+++ src/games/diceroller/dice.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { BUTTONS, MONTHS } from './config/constants';
-import './dice.css';
 import {
     updateN,
     updateMod,
     updateLog
 } from '../../redux/actions/diceActions';
+import './dice.css';
 
 class Dice extends React.Component {
     static defaultProps = {
@@ -29,7 +29,7 @@ class Dice extends React.Component {
     };
 
     componentDidMount() {
-        document.getElementById('d4').focus();
+        this.d4.focus();
     }
 
     timeStamp = () => {
@@ -41,39 +41,34 @@ class Dice extends React.Component {
         const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
         const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
         const seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-        return '[' + month + ' ' + day + ', ' + hours + ':' + minutes + ':' + seconds + ' ' + ampm + ']';
+        return `[${ month } ${ day }, ${ hours }:${ minutes }:${ seconds } ${ ampm }]`;
     }
 
     dieRoll = (event) => {
-        this.props.updateN(Number(document.getElementById('n').value));
-        this.props.updateMod(Number(document.getElementById('mod').value));
-
-        const d = event.target.innerHTML.slice(1);
-        let mod = this.props.mod;
+        const dice = event.target.innerHTML.slice(1);
         const rolls = [];
+        const n = this.props.n;
+        let mod = this.props.mod;
 
-        for (let i = 0; i < this.props.n; i++) {
-            rolls[i] = Math.floor(Math.random() * d) + 1;
+        for (let index = 0; index < n; index++) {
+            rolls[index] = Math.floor(Math.random() * dice) + 1;
         }
 
-        let rollsum = 0;
-        for (let j = 0; j < this.props.n; j++) {
-            rollsum += rolls[j];
+        let rollSum = 0;
+        for (let accumulator = 0; accumulator < n; accumulator++) {
+            rollSum += rolls[accumulator];
         }
 
-        const total = rollsum + Number(mod);
+        const total = rollSum + Number(mod);
 
-        if (
-            this.props.mod === Number(0)
-            || this.props.mod === String(0)
-        ) {
+        if (mod === Number(0) || mod === String(0)) {
             mod = '';
-        } else if (this.props.mod > 0) {
-            mod = '+' + this.props.mod;
+        } else if (mod > 0) {
+            mod = '+' + mod;
         }
 
         const timestamp = this.timeStamp();
-        const result = `<li tabindex='0'> ${ timestamp } You rolled <strong> ${ this.props.n }d${ d }${ mod } </strong> for <strong> ${ total } </strong></li>`;
+        const result = `<li tabindex='0'> ${ timestamp } You rolled <strong> ${ this.props.n }d${ dice }${ mod } </strong> for <strong> ${ total } </strong></li>`;
         const newResult = result + this.props.log;
         this.props.updateLog(newResult);
     }
@@ -100,27 +95,24 @@ class Dice extends React.Component {
                 : null;
 
             if (currButton === this.props.buttons.length - 1) {
-                this.props.updateMod(Number(event.target.value));
                 if (event.target.value.charAt(0) === '+') {
                     const result = '+' + (Number(event.target.value.slice(1)) + 1);
                     if (result === '+11') {
-                        document.getElementById('mod').selectedIndex = document.getElementById('mod').length - 1;
+                        this.mod.selectedIndex = this.mod.length - 1;
                     } else {
-                        document.getElementById('mod').value = '+' + (Number(event.target.value.slice(1)) + 1);
+                        this.mod.value = '+' + (Number(event.target.value.slice(1)) + 1);
                     }
                 } else if (
                     event.target.value === Number(0)
                     || event.target.value === String(0)
                 ) {
-                    document.getElementById('mod').value = '+1';
+                    this.mod.value = '+1';
                 } else {
-                    document.getElementById('mod').value = (Number(event.target.value) + 1);
+                    this.mod.value = (Number(event.target.value) + 1);
                 }
             }
 
-            return prevButton
-                ? document.getElementById(prevButton).focus()
-                : false;
+            return prevButton ? this[prevButton].focus() : false;
         case 39: // Right
             currButton = this.props.buttons.indexOf(event.target.id);
             nextButton = this.props.buttons[currButton + 1]
@@ -128,21 +120,18 @@ class Dice extends React.Component {
                 : null;
 
             if (currButton === 0) {
-                this.props.updateN(Number(event.target.value));
-                document.getElementById('n').value -= 1;
+                this.n.value -= 1;
             }
 
-            return nextButton
-                ? document.getElementById(nextButton).focus()
-                : false;
+            return nextButton ? this[nextButton].focus() : false;
         case 40: // Down
-            if (document.activeElement.className === 'diceroll') {
-                document.getElementById('logbutton').focus();
+            if (event.target.className === 'diceroll') {
+                this.logButton.focus();
             }
             break;
         case 38: // Up
             if (event.target.id === 'logbutton') {
-                document.getElementById('d4').focus();
+                this.d4.focus();
             }
             break;
         default:
@@ -159,6 +148,7 @@ class Dice extends React.Component {
 
     clearLog = () => {
         this.props.updateLog('');
+        this.d4.focus();
     }
 
     render() {
@@ -167,12 +157,10 @@ class Dice extends React.Component {
                 <p className="nobr">n</p>
                 <select
                     id="n"
-                    // TODO: Clean up this logic
+                    ref={ (elem) => this.n = elem }
                     defaultValue={ this.props.n }
-                    onClick={ this.setN }
                     onChange={ this.setN }
                     onKeyDown={ this.handleKeyPress }
-
                 >
                     <option>1</option>
                     <option>2</option>
@@ -186,18 +174,17 @@ class Dice extends React.Component {
                     <option>10</option>
                 </select>
 
-                <button id="d4" className="diceroll" onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d4</button>
-                <button id="d6" className="diceroll" onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d6</button>
-                <button id="d8" className="diceroll" onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d8</button>
-                <button id="d10" className="diceroll" onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d10</button>
-                <button id="d12" className="diceroll" onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d12</button>
-                <button id="d20" className="diceroll" onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d20</button>
+                <button id="d4" className="diceroll" ref={ (elem) => this.d4 = elem } onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d4</button>
+                <button id="d6" className="diceroll" ref={ (elem) => this.d6 = elem } onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d6</button>
+                <button id="d8" className="diceroll" ref={ (elem) => this.d8 = elem } onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d8</button>
+                <button id="d10" className="diceroll" ref={ (elem) => this.d10 = elem } onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d10</button>
+                <button id="d12" className="diceroll" ref={ (elem) => this.d12 = elem } onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d12</button>
+                <button id="d20" className="diceroll" ref={ (elem) => this.d20 = elem } onKeyDown={ this.handleKeyPress } onClick={ this.dieRoll }>d20</button>
 
                 <select
                     id="mod"
-                    // TODO: Clean up this logic
+                    ref={ (elem) => this.mod = elem }
                     defaultValue={ this.props.mod }
-                    onClick={ this.setMod }
                     onChange={ this.setMod }
                     onKeyDown={ this.handleKeyPress }
                 >
@@ -227,12 +214,15 @@ class Dice extends React.Component {
 
                 <p className="nobr">mod</p>
 
-                <div
-                    id="logcontainer"
-                    onKeyDown={ this.handleKeyPress }
-                    style={ { overflowY: 'scroll' } }
-                >
-                    <button id="logbutton" onClick={ this.clearLog }>Clear Log</button>
+                <div id="logcontainer">
+                    <button
+                        id="logbutton"
+                        ref={ (elem) => this.logButton = elem }
+                        onClick={ this.clearLog }
+                    >
+                        Clear Log
+                    </button>
+
                     <ul id="log">
                         { this.renderLog() }
                     </ul>
