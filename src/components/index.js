b628bb5487bbfe8ce6cfb1f08eacfb907380c7e7
jerrolb <jerrol.brooks@nagrastar.com>
2020-05-22 12:35:46 -0600

    remove unused footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import Header from './Header';
 import LeftSider from './LeftSider';
-import Footer from './Footer';
 import About from './About';
 
 export {
     Header,
     LeftSider,
-    Footer,
     About
 };
